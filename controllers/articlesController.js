cada9c38cb8e5f1280d98987887d2046fd08f12b
jwong1219 <jwong1219@gmail.com>
2017-11-20 13:00:28 -0800

    backend done, starting on react front-end now

---
+++ controllers/articlesController.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: (req, res) => {
+    db.Article
+      .find()
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: (req, res) => {
+    db.Article
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: (req, res) => {
+    db.Article
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+}
